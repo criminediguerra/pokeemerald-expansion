03c128d2d111f80926a785494861d59f65e18983
Zimmermann Gyula <graiondilach@hotmail.com>
2024-05-05 12:15:42 +0200

    Testcase.

--- src/data/apricorns.h
+++ src/data/apricorns.h
@@ -24,4 +24,16 @@ const struct ApricornTree gApricornTrees[APRICORN_TREE_COUNT] =
         .isSapling = FALSE,
         .apricornType = APRICORN_RED,
     },
+
+    [APRICORN_TREE_ROUTE101_RED_SAPLING] =
+    {
+        .isSapling = TRUE,
+        .apricornType = APRICORN_RED,
+    },
+
+    [APRICORN_TREE_ROUTE101_WHITE_TREE] =
+    {
+        .isSapling = FALSE,
+        .apricornType = APRICORN_WHITE,
+    },
 };
